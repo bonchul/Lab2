6c365a71f76ef7a5a16871ce7e2fc45f0354b5be
bonchul <bonchul1208@snu.ac.kr>
2026-04-08 13:33:22 +0900

    modify func

--- main.cpp
+++ main.cpp
@@ -7,16 +7,23 @@ int N,M;
 vector<int> vec;
 
 void func(int cnt) {
+    if (cnt == M) {
+        for (int i = 0; i < vec.size(); i++) {
+            cout << vec[i] << ' ';
+        }
+        cout << '\n';
+        return;
+    }
 
-
-
-
-
-
-
+    for (int i = 1; i <= N; i++) {
+        vec.push_back(i);
+        func(cnt + 1);
+        vec.pop_back();
+    }
 }
 
 int main() {
    cin >> N >> M;
    func(0);
+   return 0;
 }
